21659648cf5e1acced58ddd2ef9f17326d9cdb87
pprppt <mplee110698@gmail.com>
2024-05-12 22:34:38 +0800

    Feat: SFX Test

---
+++ Jame-Gam-Entry/Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public Slider musicSlider, sfxSlider, masterSlider;
+    public Sound[] musicSounds,sfxSounds;
+    public AudioSource musicSource,sfxSource;
+    public float volumeScale;
+
+    private void Awake(){
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start(){
+        setVolume(masterSlider.value);
+        PlayMusic("BGM");
+    }
+
+    public void setVolume(float scale){
+        volumeScale=scale;
+        Debug.Log("master volume: "+volumeScale);
+        setMusicVolume(musicSlider.value);
+        setSFXVolume(sfxSlider.value);
+    }
+    public void setMusicVolume(float volume){
+        musicSource.volume=volume*volumeScale;
+        Debug.Log("music volume: "+musicSource.volume);
+    }
+    public void setSFXVolume(float volume){
+        sfxSource.volume=volume*volumeScale;
+        Debug.Log("sfx volume: "+sfxSource.volume);
+    }
+
+    public void PlayMusic(string name){
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if(s!=null){
+            musicSource.clip=s.clip;
+            musicSource.Play();
+        }else{
+            Debug.Log("Audio Clip Not Found");
+        }
+    }
+
+    public void PlaySFX(string name){
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if(s!=null){
+            sfxSource.clip=s.clip;
+            sfxSource.Play();
+        }else{
+            Debug.Log("Audio Clip Not Found");
+        }
+    }
+}
---
+++ Jame-Gam-Entry/Assets/_Scripts/SceneManagement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public GameObject PauseCanvas;
+    public void StartGame(){
+        Time.timeScale = 1;
+        PauseCanvas.SetActive(false);
+        SceneManager.LoadScene("SFXTest");
+    }
+    
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            TogglePause();
+        }
+    }
+    public void TogglePause(){
+        Debug.Log("pausing...");
+        Time.timeScale = PauseCanvas.activeSelf ? 1 : 0;
+        PauseCanvas.SetActive(!PauseCanvas.activeSelf);
+    }
+}
